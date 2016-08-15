64d8ab8a8509be40b27a188f974580615e96c299
Frederico Rhae <fredrhae@gmail.com>
2016-08-15 00:16:57 -0300

    First questions solved

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+
+def ftoc fahreinheit
+	celsius = (fahreinheit - 32.0)*(5.0/9.0)
+end
+
+def ctof celsius
+	fahreinheit = (celsius*(9.0/5.0)) + 32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add number1, number2
+	addition = number1 + number2
+end
+
+def subtract number1, number2
+	subtraction = number1 - number2
+end
+
+def sum numbers
+	sum = numbers.inject(0,:+)
+end
+
+def multiply number1, number2
+	multiplication = number1 * number2
+end
+
+def multiply_many  numbers
+	multiplication = numbers.inject(:*)
+end
+
+def power number1, number2
+	power = number1 ** number2
+end
+
+def factorial number
+	factorial = (1..number).inject(1,:*)
+end
