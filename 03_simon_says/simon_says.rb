abb578211e65d7f23577c16f247951ed8d0aec6f
Frederico Rhae <fredrhae@gmail.com>
2016-08-15 21:43:29 -0300

    Did the third question

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+def echo phrase
+	phrase
+end
+
+def shout phrase
+	phrase.upcase
+end
+
+def repeat phrase, times=2
+	([phrase] * times).join(' ')
+end
+
+def start_of_word word, position
+	word.to_s[0..position - 1]
+end
+
+def first_word phrase
+	phrase.split(' ')[0]
+end
+
+def titleize words
+	processed_words = []
+	first = true
+	words.split(' ').each do |word|
+		if( first || (not ["and","the", "of", "over"].include? word))
+			processed_words << word.capitalize
+			first = false
+		else
+			processed_words << word
+		end
+	end
+	processed_words.map { |s| "#{s}" }.join(' ')
+end
+
