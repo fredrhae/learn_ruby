fe704c75fc92a5ce7b682c720697e0f13ee45dbb
Frederico Rhae <fredrhae@gmail.com>
2016-08-16 21:36:15 -0300

    Added the solution to question five

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,34 @@
 class Book
-# write your code here
+
+	LITTLE_WORDS = %w(the a an and in of)
+
+	def title
+		@title
+	end
+
+	def title=(str)
+		@title = capitalize_title(str)
+	end
+ 
+	def capitalize_title str
+		array = str.split(' ')
+		first = true
+		array.each do |word|
+			if(not little_word?(word)) || (first && the_word?(word))
+				word.capitalize!
+				if(first)
+					first = false
+				end
+			end
+		end
+		array.map {|word| "#{word}"}.join(' ')
+	end
+
+	def little_word? word
+		LITTLE_WORDS.include? word
+	end
+
+	def the_word? word
+		word == 'the'
+	end
 end
